1425aa0e94fcf4d21bde35f23d0a0f53b651d06e
Yojairo Morales <yojairo@stocktondynamics.com>
2024-04-02 11:52:39 -0700

    Initial Commit

---
+++ Momento/MomentoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentoApp.swift
+//  Momento
+//
+//  Created by Yojairo Morales on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MomentoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
